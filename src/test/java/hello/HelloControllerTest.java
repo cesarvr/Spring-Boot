cea3e405d809f9d4a267be7d961e5ae89f49a818
cesar <cvaldezr@redhat.com>
2018-03-09 13:03:27 +0100

    Fixing failing test

--- src/test/java/hello/HelloControllerTest.java
+++ src/test/java/hello/HelloControllerTest.java
@@ -25,7 +25,7 @@ public class HelloControllerTest {
     @Test
     public void getHello() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isNotAcceptable())
+                .andExpect(status().isOk())
                 .andExpect(content().string(equalTo("Greetings from Spring Boot!")));
     }
 }

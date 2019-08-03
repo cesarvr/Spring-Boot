be2da0f68523de5ff777fd5ca2e0192b33f2081d
cesar <cesar@localhost.localdomain>
2019-08-03 05:41:46 +0100

    fixing boot problems

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
 import org.springframework.context.ApplicationContext;

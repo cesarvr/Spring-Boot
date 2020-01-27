d5c08ed27ea3213c44a9409080bc735f48e6de6f
Cesar Valdez <cesar.valdez@agriculture.gov.ie>
2020-01-27 12:27:00 +0000

    fix

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-oc new-build --name=$1 --binary=true &&
-oc create dc $1 --image=$1 &&
-oc expose dc $1 &&
-oc expose svc $1 
--- jenkins/install.sh
+++ jenkins/install.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
 oc new-build nodejs~https://github.com/cesarvr/Spring-Boot --name=build-$1 --strategy=pipeline
-oc set env dc/jenkins https_proxy=http://vsdbahlprxy1:8080 && oc set env dc/jenkins http_proxy=http://vsdbahlprxy1:8080
-oc set env bc/my-application https_proxy=http://vsdbahlprxy1:8080 APPLICATION_NAME=$1
+oc set env bc/my-application https_proxy=http://vsdbahlprxy1:8080 APPLICATION_NAME=$1 PROXY=vsdbahlprxy1

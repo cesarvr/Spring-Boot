c1cd135175d80407abf433b24d0643635845ecbc
Cesar Valdez <cesar.valdez@agriculture.gov.ie>
2020-01-27 12:08:02 +0000

    fixing svc creation

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-oc new-build --name=$1 --binary=true &&
-oc create dc $1 --image=$1 &&
-oc expose dc $1 --port=8080 &&
-oc expose svc $1
+oc new-build --name=$1 --binary=true || true &&
+oc create dc $1 --image=$1 || true &&
+oc expose dc $1 --port=8080 || true &&
+oc expose svc $1 || true

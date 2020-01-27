fa0d2251f3d085425f71ad79f40109822f2851e9
Cesar Valdez <cesar.valdez@agriculture.gov.ie>
2020-01-27 14:58:34 +0000

    fixing Jenkinsfile

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-oc new-build --name=$1 --binary=true || true &&
+oc new-build redhat-openjdk18-openshift:1.2 --name=$1 --binary=true || true &&
 oc create dc $1 --image=$1 || true &&
 oc expose dc $1 --port=8080 || true &&
 oc expose svc $1 || true

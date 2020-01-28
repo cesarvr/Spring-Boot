6a0b9dfdcebdaecc0ae9bd592d5581945b82579f
Cesar Valdez <cesar.valdez@agriculture.gov.ie>
2020-01-28 09:16:16 +0000

    fix this thing

--- jenkins/install.sh
+++ jenkins/install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-oc new-build nodejs~https://github.com/cesarvr/Spring-Boot --name=build-$1 --strategy=pipeline
-oc set env bc/my-application https_proxy=http://vsdbahlprxy1:8080 APPLICATION_NAME=$1 PROXY=vsdbahlprxy1
+oc new-build nodejs~$2 --name=build-$1 --strategy=pipeline
+oc set env bc/build-$1 APPLICATION_NAME=$1 PROXY=vsdbahlprxy1
